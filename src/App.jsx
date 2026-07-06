bb77c02e2ea368802728a17bef2d745545848997
manprajapatie <manprajapatie@gmail.com>
2026-07-06 07:50:47 +0530

    chore : add Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
 
   return (
     <>
-     <h1 className='text-9xl text-blue-500 font-extrabold'>
-      It's Man
-     </h1>
+      <Header />
+      <AppRoutes />
+      <Footer />
     </>
   )
 }
